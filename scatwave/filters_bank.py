fix wavelets normalization

scatwave/filters_bank.py
@@ -112,28 +112,48 @@ def gabor_2d(M, N, sigma, theta, xi, slant=1.0, offset=0, fft_shift=None):
 
 
 def solid_harmonic_3d(M, N, O, sigma, l, fourier=True):
+    """Computes solid harmonic wavelets in Fourier or signal space.
+
+    Input args:
+        M, N, O: integers, shape of the grid
+        sigma: float, width of the wavelets
+        l: integer, degree of the harmonic
+        fourier: boolean, compute wavelet in fourier space
+                 or in signal space
+
+    Returns:
+        solid_harm: 4D tensors of shape (2l+1, M, N, O). The
+                    tensor is ifftshifted such that the point 0 in
+                    signal space or in Fourier space is at
+                    [m, 0, 0, 0] for m = 0 ... 2*l+1
+    """
     solid_harm = np.zeros((2*l+1, M, N, O), np.complex64)
+    grid = np.mgrid[-M//2:M//2, -N//2:N//2, -O//2:O//2].astype('float32')
+
     if fourier:
-        grid = np.mgrid[-np.pi:np.pi:1j*M, -np.pi:np.pi:1j*N, -np.pi:np.pi:1j*O]
-        r_square = (grid**2).sum(0)
-        polynomial_gaussian = r_square**(0.5*l) * sigma**l * np.exp(-0.5 * r_square * sigma**2)
-    else:
-        grid = np.mgrid[-M//2:M//2, -N//2:N//2, -O//2:O//2]
-        r_square = (grid**2).sum(0)
-        polynomial_gaussian = r_square**(0.5*l) / sigma**l * np.exp(-0.5 * r_square / sigma**2)
+        grid[0] *= 2 * np.pi / M
+        grid[1] *= 2 * np.pi / N
+        grid[2] *= 2 * np.pi / O
+        sigma = 1. / sigma
+
+    r_square = (grid**2).sum(0)
+    polynomial_gaussian = r_square**(0.5*l) / sigma**l * np.exp(-0.5 * r_square / sigma**2)
 
     polar, azimuthal = get_3d_angles(grid)
 
     for i_m, m in enumerate(range(-l, l+1)):
         solid_harm[i_m] = sph_harm(m, l, azimuthal, polar) * polynomial_gaussian
 
-    norm_factor = 1.
     if l % 2 == 0:
-        norm_factor /= sigma**3 * np.pi * np.sqrt(l+0.5) * double_factorial(l+1)
+        norm_factor = 1. / (np.pi * np.sqrt(l+0.5) * double_factorial(l+1))
     else :
-        norm_factor /= sigma**3 * 2**(0.5*(l+1)) * np.sqrt(np.pi*(2*l+1)) * factorial((l+1)/2)
+        norm_factor = 1. / (2**(0.5*(l+1)) * np.sqrt(np.pi*(2*l+1)) * factorial((l+1)/2))
+
     if fourier:
-        norm_factor *= 4 * np.pi * (-1j)**l
+        norm_factor *= (2 * np.pi)**1.5 * (-1j)**l
+    else:
+        norm_factor /= sigma**3
+
     solid_harm *= norm_factor
 
     return np.fft.ifftshift(solid_harm)

scatwave/utils.py
@@ -69,7 +69,7 @@ def get_3d_angles(cartesian_grid):
     """
     z, y, x = cartesian_grid
     azimuthal = np.arctan2(y, x)
-    rxy = np.sqrt(x ** 2 + y ** 2)
+    rxy = np.sqrt(x**2 + y**2)
     polar = np.arctan2(z, rxy) + np.pi / 2
     return polar, azimuthal
 