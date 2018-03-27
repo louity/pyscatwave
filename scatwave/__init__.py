88a59550f9d8078680f783721006b124a6614ee6
Louis Thiry <biboundai@homail.fr>
2018-03-27 17:53:15 +0200

    move to pytorch 0.3.1, remove 2D, implement GPU

--- scatwave/__init__.py
+++ scatwave/__init__.py
@@ -2,8 +2,8 @@
 Authors: Eugene Belilovsky, Edouard Oyallon and Sergey Zagoruyko
 All rights reserved, 2017.
 """
-__all__ = ['Scattering']
+__all__ = ['SolidHarmonicScattering']
 
 
-from .scattering import Scattering
+from .scattering import SolidHarmonicScattering
 from . import utils
